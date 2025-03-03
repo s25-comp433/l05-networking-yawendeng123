c4379bd1b805f7c09ab0f0a3b463a0891c5e6908
Yawen Deng <yawend@ad.unc.edu>
2025-03-02 21:39:22 -0500

    UNC Basketball Completed

--- BasketballGames/BasketballGames/ContentView.swift
+++ BasketballGames/BasketballGames/ContentView.swift
@@ -7,15 +7,76 @@
 
 import SwiftUI
 
+struct Game: Codable {
+    var id: Int
+    var team: String
+    var opponent: String
+    var date: String
+    var score: GameScores
+    var isHomeGame: Bool
+}
+
+struct GameScores: Codable {
+    var unc: Int
+    var opponent: Int
+}
+
 struct ContentView: View {
+    @State private var games = [Game]()
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            List(games, id: \.id) { game in
+                HStack {
+                    VStack(alignment: .leading) {
+                        Text("\(game.team) vs \(game.opponent)")
+                            .font(.headline)
+                            .fontWeight(.regular)
+                            .foregroundStyle(.primary)
+                        
+                        Text(game.date)
+                            .font(.footnote)
+                            .foregroundStyle(.secondary)
+                    }
+                    
+                    Spacer()
+                    
+                    VStack(alignment: .trailing) {
+                        Text("\(game.score.unc) - \(game.score.opponent)")
+                            .font(.headline)
+                            .fontWeight(.regular)
+                            .foregroundStyle(.primary)
+                        
+                        var location = game.isHomeGame ? "Home" : "Away"
+                        
+                        Text("\(location)")
+                            .font(.footnote)
+                            .foregroundStyle(.secondary)
+                    }
+                }
+            }
+            .navigationTitle("UNC Basketball")
+            .task {
+                await loadData()
+            }
         }
-        .padding()
+    }
+    
+    func loadData() async {
+        guard let url = URL(string: "https://api.samuelshi.com/uncbasketball") else {
+            print("Invalid URL")
+            return
+        }
+        
+        do {
+            let (data, _) = try await URLSession.shared.data(from: url)
+                    
+            if let decodedResponse = try? JSONDecoder().decode([Game].self, from: data) {
+                games = decodedResponse
+            }
+        } catch {
+            print("Invalid data")
+        }
     }
 }
 
